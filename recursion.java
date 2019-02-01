fc6df54d6c63bf71bcf819477e093928808d55c0
wyao1 <wyao1@stuy.edu>
2019-01-31 22:52:55 -0500

    started working on helper method for tail recursive fib

--- recursion.java
+++ recursion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class recursion{
 
     public static double sqrtHelp(double n, double tolerance, double g){
@@ -7,22 +9,30 @@ public class recursion{
       else{
         return sqrtHelp(n, tolerance, (n/g + g)/2);
       }
-    }
-  
+    }  
     public static double sqrt(double n, double tolerance){
-      
+      return sqrtHelp(n, tolerance, 1);
     }
 
-    /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 1; fib(1) = 1; fib(5) = 5
-     *precondition: n is non-negative
-     */
-    public static int fib(int n){
+    public int fibonacciHelp(int n, int ans) {
+      if (n == 0){
+        return 0;
+      }
+      if (n == 1){
+        return 1;     //NEED BASE CASE + NEED COUNTER?
+      }
+      else{
+        ans = ans-1 + ans-2;
+        return fibonacciHelp(n,ans);
+      }
+    }
 
+    public static int fib(int n){
+      return fibonacciHelp(n, 0);
     }
 
     /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
-    }
+ //   public static ArrayList<Integer> makeAllSums(){
+   // }
 
 }
